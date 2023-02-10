4d26b05dd185c878b654ad05f3b11544473ec35c
Robert Bielicki <robertbielickiwet@gmail.com>
2023-02-10 20:40:58 +0100

    first commit, created project, app and models

---
+++ images/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+def user_directory_path(instance, filename):
+    return 'images/{filename}/'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Images(models.Model):
+
+    options = (
+        ('active', 'Active'),
+        ('deactivated', 'Deactivated'),
+    )
+    
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
+    title = models.CharField(max_length=250)
+    alt = models.TextField(null=True)
+    image = models.ImageField(upload_to=user_directory_path, default='posts/default.jpg')
+    slug = models.SlugField(max_length=250, unique_for_date='created')
+    created = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='author')
+    status = models.CharField(max_length=50, choices=options, default='active')
