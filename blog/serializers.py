4e60b701e8bad8f6a8d26248831758f34599e311
Robert Bielicki <robertbielickiwet@gmail.com>
2023-02-14 15:06:34 +0100

    kosmetyka

--- blog/serializers.py
+++ blog/serializers.py
@@ -2,25 +2,18 @@ from rest_framework import serializers
 from blog.models import Images, Profile
 
 
+def user_directory_path(instance, filename):
+    return 'images/{0}'.format(filename)
 
-
-class ImagesSerializer(serializers.Serializer):
-
-    #creator = serializers.ReadOnlyField(source='author.username')
-    #profile = Images.objects.all()
+class ImagesSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(
+        max_length=None,
+        use_url=True
+    )
 
     class Meta:
         model = Images
-        fields = '__all__'
+        fields = ['title', 'image']
 
-
-
-
-#class ImagesSerializer(serializers.ModelSerializer):
-#
-#    creator = serializers.ReadOnlyField(source='author.username')
-#    profile = Images.objects.all()[0]
-#
-#    class Meta:
-#        model = Images
-#        fields = ('author', 'creator', 'title', 'image')
+    def create(self, validated_data):
+        return Images.objects.create(**validated_data)
--- blog/views.py
+++ blog/views.py
@@ -12,15 +12,12 @@ from rest_framework import status
 class ImagesViewSet(APIView):
     permission_classes = (IsAuthenticated,)
 
-    
     def get(self, request, format=None):
         user = self.request.user
         membership = user.profile.membership
 
         small_images_links =[str(img.thumbnail_200.url) for img in Images.objects.filter(author=user)]
         medium_images_links =[str(img.thumbnail_400.url) for img in Images.objects.filter(author=user)]
-        #small_images_links = profile.thumbnail_200.url                            #small_images = Images.objects.filter(author=user).values('id', 'image')
-        #medium_images_links = profile.thumbnail_400.url                           #medium_images = Images.objects.filter(author=user).values('id', 'image')
 
 
         content = {
@@ -34,40 +31,10 @@ class ImagesViewSet(APIView):
 
         return Response(content)
 
-    def post(self, request, format=None):
+    def post(self, request):
         serializer = ImagesSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-########################################################################
-########################################################################
-
-print('\n\n')
-
-
-#print(Profile.objects.filter(id=1))
-#user_obj = User.objects.filter(id=2).first()
-#profile_obj = Profile.objects.all().first()
-#print(profile_obj.membership)
-#print(profile_obj.user)
-#print(user_obj == profile_obj.user)
-#filtered = Images.objects.filter(author=2)
-#print(filtered)
-
-#profile = Images.objects.all()
-#print(profile.thumbnail_200.url)    # > /media/CACHE/images/982d5af84cddddfd0fbf70892b4431e4.jpg
-#print(profile.thumbnail_400.url)
-#
-#
-#print(f'WIDTH: {profile.thumbnail_200.width}')
-#print(f'HEIGHT: {profile.thumbnail_200.height}')
-#
-#print(f'WIDTH: {profile.thumbnail_400.width}')
-#print(f'HEIGHT: {profile.thumbnail_400.height}')
-
-
-print('\n\n')
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
