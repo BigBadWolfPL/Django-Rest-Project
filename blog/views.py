756626306bfa5e2e42607a1f30315c9241141e40
Robert Bielicki <robertbielickiwet@gmail.com>
2023-02-16 10:42:34 +0100

    Working postman POST/GET with small/medium thumbnail links

--- blog/views.py
+++ blog/views.py
@@ -14,23 +14,6 @@ class ImagesViewSet(APIView):
     #permission_classes = [IsAuthenticated]
     parser_classes = [MultiPartParser, FormParser]
 
-    def get(self, request, format=None):
-        #user = self.request.user
-        #membership = user.profile.membership
-
-        #small_images_links =[str(img.thumbnail_200.url) for img in Images.objects.filter(author=user)]
-        #medium_images_links =[str(img.thumbnail_400.url) for img in Images.objects.filter(author=user)]
-
-        content = {
-            'user': str(request.user),
-            #'small_images_links': small_images_links,
-            #'medium_images_links': medium_images_links,
-            #'membership_BASIC': membership == "BASIC",
-            #'membership_PREMIUM': membership == "PREMIUM",
-            #'membership_ENTERPRISE': membership == "ENTERPRISE",
-        }
-        return Response(content)
-
     def post(self, request, format=None):
         serializer = ImagesSerializer(data=request.data)
         if serializer.is_valid():
@@ -38,3 +21,23 @@ class ImagesViewSet(APIView):
             return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
         else:
             return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, format=None):
+        #user = self.request.user
+        #membership = user.profile.membership
+
+        #small_images_links =[str(img.thumbnail_200.url) for img in Images.objects.filter(author=user)]
+        #medium_images_links =[str(img.thumbnail_400.url) for img in Images.objects.filter(author=user)]
+        small_images_links =[str(img.thumbnail_200.url) for img in Images.objects.all()]
+        medium_images_links =[str(img.thumbnail_400.url) for img in Images.objects.all()]
+
+        content = {
+            #'user': str(request.user),
+            'small_images_links': small_images_links,
+            'medium_images_links': medium_images_links,
+            ##'membership_BASIC': membership == "BASIC",
+            ##'membership_PREMIUM': membership == "PREMIUM",
+            #'membership_ENTERPRISE': membership == "ENTERPRISE",
+        }
+        return Response(content)
+small_images_links =[str(img.thumbnail_200.url) for img in Images.objects.all()]
+print(small_images_links)
