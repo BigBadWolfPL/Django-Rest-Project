241c2333099da02ebb268d5f1291ee4c68bee5cc
Robert Bielicki <robertbielickiwet@gmail.com>
2023-02-16 13:30:18 +0100

    added properly working username and membership filtering and displaying results

--- blog/views.py
+++ blog/views.py
@@ -19,25 +19,41 @@ class ImagesViewSet(APIView):
             return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
         else:
             return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
     def get(self, request, format=None):
 
         if self.request.user.id is None:
             user = f"{self.request.user} --> PLEASE LOGIN // PROSZĘ SIĘ ZALOGOWAĆ ;) <--"
-            small_images_links =[str(img.thumbnail_200.url) for img in Images.objects.all()]  # Tylko do podglądu >>> należy się zalogować w postman
-            medium_images_links =[str(img.thumbnail_400.url) for img in Images.objects.all()]
-        else:
-            user = self.request.user
-            membership = user.profile.membership #trzeba się zalogować w postman bo wywala błąd (AnonymousUser ...) stąd ten warunek if/ else
-            small_images_links =[str(img.thumbnail_200.url) for img in Images.objects.filter(author=user)]
-            medium_images_links =[str(img.thumbnail_400.url) for img in Images.objects.filter(author=user)]
-
-        content = {
+            small_images_links =[str(img.thumbnail_200.url) for img in Images.objects.all()]  # Tylko do podglądu // usunąć >>> należy się zalogować w postman
+            medium_images_links =[str(img.thumbnail_400.url) for img in Images.objects.all()] # Tylko do podglądu // usunąć >>> należy się zalogować w postman
+            
+            content = {
             'user': str(request.user),
             'small_images_links': small_images_links,
             'medium_images_links': medium_images_links,
             'membership_None': f"{user}",
-            #'membership_BASIC': membership == "BASIC",
-            #'membership_PREMIUM': membership == "PREMIUM",
-            #'membership_ENTERPRISE': membership == "ENTERPRISE",
-        }
+            }
+        else:
+            ### BASIC MEMBERSHIP ###
+            user = self.request.user
+            membership = user.profile.membership
+            small_images_links =[str(img.thumbnail_200.url) for img in Images.objects.filter(author=user)]
+            content = {
+                'user': str(request.user),
+                'your_membership': membership,
+                'small_images_links': small_images_links,
+                }
+            ### PREMIUM MEMBERSHIP ###
+            if membership == "PREMIUM":
+                medium_images_links =[str(img.thumbnail_400.url) for img in Images.objects.filter(author=user)] 
+                content['medium_images_links'] = medium_images_links
+            ### ENTERPRISE MEMBERSHIP ###
+            if membership == "ENTERPRISE":
+                medium_images_links =[str(img.thumbnail_400.url) for img in Images.objects.filter(author=user)] 
+                binary_images_links = "link to binary ..."
+                oryginal_images_links = "link to oryginal image ..."
+                content['medium_images_links'] = medium_images_links
+                content['binary_images_links'] = binary_images_links
+                content['oryginal_images_links'] = oryginal_images_links
+
         return Response(content)
