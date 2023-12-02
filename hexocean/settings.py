371ebe1e2b00244aa02eee64da2f61ec464305f3
Robert Bielicki <robertbielickiwet@gmail.com>
2023-12-02 16:41:36 +0100

    removed docker

--- hexocean/settings.py
+++ hexocean/settings.py
@@ -92,12 +92,8 @@ WSGI_APPLICATION = 'hexocean.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'restDB',
     }
 }
 
